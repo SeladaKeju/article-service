8bf4528ae79722e868c329ff1f5e5ec76036beb0
SeladaKeju <rizqiadit2430@gmail.com>
2026-09-08 21:40:03 +0700

    chore: add health endpoint

---
+++ cmd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func router() *gin.Engine {
+	r := gin.Default()
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{"status": "ok"})
+	})
+	return r
+}
+
+func main() {
+	router().Run()
+}
